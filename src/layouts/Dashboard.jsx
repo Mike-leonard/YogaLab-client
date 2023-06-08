f82c12dac8959b0fff3a054c6c6fe97fd18ee57d
leonard <mortuza.7@gmail.com>
2023-06-08 10:23:42 +0600

    dashboard width related issue fixed

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -3,11 +3,11 @@ import { FaBars, FaBookOpen, FaTachometerAlt, FaChalkboardTeacher, FaPeopleCarry
 import useTheme from '../hooks/useTheme';
 import { NavLink, Outlet } from 'react-router-dom';
 
-
 const Dashboard = () => {
-    const { theme } = useTheme()
+    const { theme } = useTheme();
     const [showSidebar, setShowSidebar] = useState(true);
-    const isRule = "instructor"
+    const isRule = "instructor";
+
     const toggleSidebar = () => {
         setShowSidebar(!showSidebar);
     };
@@ -15,57 +15,115 @@ const Dashboard = () => {
     return (
         <div className="flex h-screen">
             {/* Sidebar */}
-            <div className={`w-${showSidebar ? '1/3' : '0'}  ${theme === 'light' ? 'bg-[#DC2C5C] text-white' : 'bg-[#030508]'} transition-all duration-300 ${showSidebar ? 'opacity-100' : 'opacity-0'}`} style={{ minWidth: '0' }}>
+            <div
+                className={`${showSidebar ? 'w-3/12' : 'w-0'
+                    } ${theme === 'light' ? 'bg-[#DC2C5C] text-white' : 'bg-[#030508]'
+                    } transition-all duration-300 ${showSidebar ? 'opacity-100' : 'opacity-0'
+                    }`}
+            >
                 {/* Sidebar Content */}
                 {/* Add your sidebar content here */}
                 <div className="mt-20">
-                    <ul className="menu p-4 w-80">
-                        {
-                            isRule === 'admin' ?
-                                <>
-                                    <li><NavLink to="/dashboard/home"><FaTachometerAlt></FaTachometerAlt>Admin Home</NavLink></li>
-                                    <li><NavLink to="/dashboard/addItem"><FaBookOpen></FaBookOpen>Manage Classes</NavLink></li>
-                                    <li><NavLink to="/dashboard/allusers"><FaUsers></FaUsers> Manage Users</NavLink></li>
-
-                                </> :
-                                <> {
-                                    isRule === 'instructor' ?
-                                        <>
-                                            <li><NavLink to="/dashboard/home"><FaHome></FaHome> Instructor Home</NavLink></li>
-                                            <li><NavLink to="/dashboard/reservations"><FaCalendarAlt></FaCalendarAlt> Add a Class</NavLink></li>
-                                            <li><NavLink to="/dashboard/history"><FaBookOpen></FaBookOpen> My Classes</NavLink></li>
-                                        </> :
-                                        <>
-
-                                            <li><NavLink to="/dashboard/home"><FaHome></FaHome> Student Home</NavLink></li>
-                                            <li><NavLink to="/dashboard/selected-classes"><FaCalendarAlt></FaCalendarAlt> My Selected Classes</NavLink></li>
-                                            <li><NavLink to="/dashboard/enrolled-classes"><FaChalkboardTeacher></FaChalkboardTeacher> My Enrolled Classes</NavLink></li>
-                                            <li><NavLink to="/dashboard/payment-history"><FaWallet></FaWallet> Payment History</NavLink></li>
-                                        </>
-                                }
-                                </>
-                        }
-
+                    <ul className="menu p-4">
+                        {isRule === 'admin' ? (
+                            <>
+                                <li>
+                                    <NavLink to="/dashboard/home">
+                                        <FaTachometerAlt /> Admin Home
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/dashboard/addItem">
+                                        <FaBookOpen /> Manage Classes
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/dashboard/allusers">
+                                        <FaUsers /> Manage Users
+                                    </NavLink>
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                {isRule === 'instructor' ? (
+                                    <>
+                                        <li>
+                                            <NavLink to="/dashboard/home">
+                                                <FaHome /> Instructor Home
+                                            </NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink to="/dashboard/reservations">
+                                                <FaCalendarAlt /> Add a Class
+                                            </NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink to="/dashboard/history">
+                                                <FaBookOpen /> My Classes
+                                            </NavLink>
+                                        </li>
+                                    </>
+                                ) : (
+                                    <>
+                                        <li>
+                                            <NavLink to="/dashboard/home">
+                                                <FaHome /> Student Home
+                                            </NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink to="/dashboard/selected-classes">
+                                                <FaCalendarAlt /> My Selected Classes
+                                            </NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink to="/dashboard/enrolled-classes">
+                                                <FaChalkboardTeacher /> My Enrolled Classes
+                                            </NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink to="/dashboard/payment-history">
+                                                <FaWallet /> Payment History
+                                            </NavLink>
+                                        </li>
+                                    </>
+                                )}
+                            </>
+                        )}
                         <div className="my-4 border-t border-white"></div>
-                        <li><NavLink to="/"><FaHome></FaHome> Home</NavLink> </li>
-                        <li><NavLink to="/instructors"><FaPeopleCarry />Instructors</NavLink></li>
-                        <li><NavLink to="/classes"><FaChalkboardTeacher />Classes</NavLink></li>
+                        <li>
+                            <NavLink to="/">
+                                <FaHome /> Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/instructors">
+                                <FaPeopleCarry /> Instructors
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/classes">
+                                <FaChalkboardTeacher /> Classes
+                            </NavLink>
+                        </li>
                     </ul>
                 </div>
             </div>
 
-
-            <div className={`w-${showSidebar ? 'full' : 'full'} mx-auto transition-all duration-300`}>
+            <div
+                className={`${showSidebar ? 'w-9/12' : 'w-full'
+                    } transition-all duration-300`}
+            >
                 {/* Main Content Header */}
-                <div className={`py-4 text-center  ${theme === 'light' ? 'bg-[#DC2C5C] text-white' : 'bg-[#030508]'}`}>
+                <div
+                    className={`fixed w-full py-4 text-center ${theme === 'light' ? 'bg-[#DC2C5C] text-white' : 'bg-[#030508]'
+                        }`}
+                >
                     {/* Add your main content header here */}
                     <h1 className="text-xl font-semibold">{isRule.toUpperCase()}</h1>
                 </div>
 
                 <Outlet />
             </div>
-
-
 
             {/* Button to show/hide sidebar */}
             <button
@@ -74,7 +132,6 @@ const Dashboard = () => {
             >
                 <FaBars />
             </button>
-            {/*    {showSidebar ? 'Hide Sidebar' : 'Show Sidebar'} */}
         </div>
     );
 };
