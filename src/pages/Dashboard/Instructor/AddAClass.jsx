480d696e7ec21d01c8857d1a35d88d333ddf1e34
leonard <mortuza.7@gmail.com>
2023-06-09 16:25:55 +0600

    instructor posting a class to db

--- src/pages/Dashboard/Instructor/AddAClass.jsx
+++ src/pages/Dashboard/Instructor/AddAClass.jsx
@@ -1,9 +1,50 @@
+import axios from "axios";
 import { useForm } from "react-hook-form";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+import useAuth from "../../../hooks/useAuth";
 
 const AddAClass = () => {
-
+    const {user} = useAuth()
     const { register, handleSubmit, reset } = useForm()
-    const onSubmit = data => {}
+    const [axiosSecure] = useAxiosSecure()
+    const img_hosting_token = import.meta.env.VITE_IMAGE_HOSTING_KEY
+    const img_hosting_url = `https://api.imgbb.com/1/upload?key=${img_hosting_token}`
+
+    console.log(user)
+
+    const onSubmit = data => {
+        const formData = new FormData()
+        formData.append('image', data.image[0])
+        axios.post(img_hosting_url, formData)
+            .then(imgResponse => {
+                if (imgResponse.data.success) {
+                    console.log('success')
+                    const imgURL = imgResponse.data.data.display_url
+                    const { class_name, available_seat, price } = data
+                    const addNewClass = {
+                        class_name, imgURL, instructor_name: user?.displayName, instructor_email: user?.email,
+                        available_seat: parseFloat(available_seat), enroll_student: 0,
+                        price: parseFloat(price), feedback: null, status: "pending"
+                    }
+                    axiosSecure.post('/classes', addNewClass)
+                        .then(data => {
+                            console.log('after posting new menu item', data.data)
+                            if (data.data.insertedId) {
+                                reset()
+                                Swal.fire({
+                                    position: 'top-end',
+                                    icon: 'success',
+                                    title: 'New class added successfully',
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                })
+                            }
+                        })
+                }
+            })
+        console.log(data)
+    }
 
 
 
@@ -16,7 +57,7 @@ const AddAClass = () => {
                         <label className="label">
                             <span className="label-text font-semibold">Class Name*</span>
                         </label>
-                        <input type="text" {...register("price", { required: true })} placeholder="Type here" className="input input-bordered w-full " />
+                        <input type="text" {...register("class_name", { required: true })} placeholder="Type here" className="input input-bordered w-full " />
                     </div>
                     <div className="form-control w-full ml-4">
                         <label className="label">
@@ -30,13 +71,13 @@ const AddAClass = () => {
                         <label className="label">
                             <span className="label-text font-semibold">Instructor Name</span>
                         </label>
-                        <input type="text" {...register("price", { required: true })} placeholder="Type here" className="input input-bordered w-full " />
+                        <input type="text" defaultValue={user?.displayName} {...register("instructor_name")} placeholder="Type here" className="input input-bordered w-full " disabled />
                     </div>
                     <div className="form-control w-full ml-4">
                         <label className="label">
                             <span className="label-text font-semibold">Instructor Email</span>
                         </label>
-                        <input type="text" {...register("price", { required: true })} placeholder="Type here" className="input input-bordered w-full " />
+                        <input type="text" defaultValue={user?.email} {...register("instructor_email")} placeholder="Type here" className="input input-bordered w-full " disabled />
                     </div>
                 </div>
                 <div className="flex my-4">
@@ -44,7 +85,7 @@ const AddAClass = () => {
                         <label className="label">
                             <span className="label-text font-semibold">Available Seat*</span>
                         </label>
-                        <input type="text" {...register("price", { required: true })} placeholder="Type here" className="input input-bordered w-full " />
+                        <input type="text" {...register("available_seat", { required: true })} placeholder="Type here" className="input input-bordered w-full " />
                     </div>
                     <div className="form-control w-full ml-4">
                         <label className="label">
